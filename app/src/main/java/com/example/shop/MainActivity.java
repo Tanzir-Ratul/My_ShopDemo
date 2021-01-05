93301c3f40cada96f3957b9141389c1e410b42f1
Abdur Rahman <abdurrahmanman1993@gmail.com>
2021-01-05 22:47:48 +0600

    ViewPagerAdapterNavi

--- app/src/main/java/com/example/shop/MainActivity.java
+++ app/src/main/java/com/example/shop/MainActivity.java
@@ -6,22 +6,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.Window;
 import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
+import com.example.shop.Adapters.ViewPagerAdapter;
+import com.example.shop.fragments.CartFragment;
+import com.example.shop.fragments.HomeFragment;
+import com.example.shop.fragments.ProfileFragment;
+import com.example.shop.fragments.WishlistFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle toggle;
     private Toolbar toolbar;
     NavigationView navigationView;
+//    ViewPager and BottomNavigationMenuView
+    private BottomNavigationView mBottomNavigation;
+    private ViewPager viewPager;
+    private ViewPagerAdapter mViewPagerAdapter;
+    MenuItem prevMenuItem;
+    ProfileFragment profileFragment;
+    WishlistFragment wishlistFragment;
+    HomeFragment homeFragment;
+    CartFragment cartFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
                 switch(item.getItemId()){
-                    ase R.id.
+//                    ase R.id.
                 }
 
                 return true;
@@ -59,8 +78,45 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Exception " + e, Toast.LENGTH_SHORT).show();
         }
 
+        //    ViewPager and BottomNavigationMenuView
 
+        mBottomNavigation = findViewById(R.id.buttom_navigation);
+        mBottomNavigation.setOnNavigationItemSelectedListener(this);
+
+        viewPager = findViewById(R.id.view_pager);
+
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                }
+                else
+                {
+                    mBottomNavigation.getMenu().getItem(0).setChecked(false);
+                }
+                Log.d("page", "onPageSelected: "+position);
+                mBottomNavigation.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = mBottomNavigation.getMenu().getItem(position);
+
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        setupViewPager(viewPager);
     }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
@@ -70,4 +126,34 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void setupViewPager(ViewPager viewPager) {
+        mViewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        homeFragment = new HomeFragment();
+        wishlistFragment = new WishlistFragment();
+        cartFragment = new CartFragment();
+        profileFragment = new ProfileFragment();
+        mViewPagerAdapter.addFragment(homeFragment);
+        mViewPagerAdapter.addFragment(wishlistFragment);
+        mViewPagerAdapter.addFragment(cartFragment);
+        mViewPagerAdapter.addFragment(profileFragment);
+        viewPager.setAdapter(mViewPagerAdapter);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment selectedFragment = null;
+        switch (item.getItemId()) {
+            case R.id.action_home:
+                viewPager.setCurrentItem(1);
+                return true;
+            case R.id.action_favorites:
+                viewPager.setCurrentItem(0);
+                return true;
+            case R.id.action_account:
+                viewPager.setCurrentItem(2);
+                return true;
+            default:
+                return false;
+        }
+    }
 }
